e2cabcba43670f01454032b1fbce2066ed5dffb9
Sana Diwan <sanadiwan2006@gmail.com>
2017-04-05 19:36:08 +0000

    webpage format

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//**Creating a server, reference to: https://community.c9.io/t/writing-a-node-js-app/1731 -Sana**//
+
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
